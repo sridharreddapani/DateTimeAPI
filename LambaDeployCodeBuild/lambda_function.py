f1b63ca5915ee183ca080f1a40d42fbc7e910bb0
Sridhar Reddapani <sridhar@Sridhars-MacBook-Pro.local>
2021-05-24 10:04:19 +1000

    Initial commit

---
+++ LambaDeployCodeBuild/lambda_function.py
@@ -0,0 +1,13 @@
+import json
+import os
+from datetime import datetime
+def lambda_handler(event, context):
+    data = {}
+    now = datetime.now().isoformat()
+    data['Name'] = os.environ["Name"]
+    data['DateTime'] = now
+    data['Version'] = "Version 1.0.1a"
+    return {
+        'statusCode': 200,
+        'body': json.dumps(data)
+    }
